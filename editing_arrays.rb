303d7466162bd0ed1be7b0d5c0ca32a6d78bcff7
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-08 18:28:39 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,7 +11,7 @@ def change_rainbow_colors
 end
 
 def add_colors
-  rainbow_colors = ["yellow","default","light_cyan"]
+  rainbow_colors = ["red", "light_red", "light_yellow"]
   rainbow_colors << "green"
   rainbow_colors << "blue"
   return rainbow_colors
